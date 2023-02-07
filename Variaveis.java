0212502c6ac44222165a345e9488d99d0a9f4cc3
luaanHD <luantoniolo94@gmail.com>
2023-02-07 15:17:12 +0000

    Salvando arquivos variáveis

--- Variaveis.java
+++ Variaveis.java
@@ -1,3 +1,31 @@
 public class Variaveis {
+    public static void main(String[] args) {
+        
     
+
+    int idade=16;
+
+    char letra='M';
+
+    boolean vivo=false;
+
+    byte quantidadeBola=7;
+
+    double salario=1302.50;
+
+    float salarioMenor=1212.47f;
+
+    short alunosPalmital=10000;
+
+    long populacaoPalmital=22000;
+
+    System.out.println(idade);
+    System.out.println(letra);
+    System.out.println(vivo);
+    System.out.println(quantidadeBola);
+    System.out.println(salario);
+    System.out.println(salarioMenor);
+    System.out.println(alunosPalmital);
+    System.out.println(populacaoPalmital);
+    }
 }
